7b5f70a406432ff0449358a4d521bc7b8feb6681
Diego ANdrès Gùzman <guzmandiegoandres12@gmail.com>
2020-08-23 20:25:45 -0500

    implementacion de modelo para reconocimiento de imagenes y fotogramas en videos

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 'use strict'
 //Importacion de librearia
+const tf = require('@tensorflow/tfjs');
+const mobilnet =require('@tensorflow-models/mobilenet');
+
 import LinealRegresionModel from './model/LinealRegresionModel';
 const data = require ('../data/data.json') ;
 
@@ -9,6 +12,7 @@ const $reloadTrainig = document.getElementById('reloadTrainig');
 const $save = document.getElementById('save');
 const $loadModel = document.getElementById('loadModel');
 const $generatePrediction = document.getElementById('generatePrediction');
+const $createModel = document.getElementById('createModel');
 
 /**
  * IMPLEMENTACION DE MODELO PARA REGRECION LINEAL
@@ -20,8 +24,11 @@ const dataCleanN2 = dataCleanN1.filter( ({precio,cuartos}) => (precio != null &&
 const dataPrepared = dataCleanN2.map(({precio,cuartos}) => [cuartos,precio] )
 const linearModel = new LinealRegresionModel(dataPrepared,'Cuartos','Presio');
 
-linearModel.visualizeInitialData();
-linearModel.createModel();
+$createModel.addEventListener('click',()=>{
+  linearModel.visualizeInitialData();
+  linearModel.createModel();
+})
+
 
 
 $stoptrainig.addEventListener('click',()=>{
@@ -51,5 +58,56 @@ $generatePrediction.addEventListener('click',async()=>{
  * IMPLEMENTACION DE MODELO PARA RECONOCIMIENTO DE IMAGES
  */
 
+let net;
+const image =document.getElementById('img');
+const $newImage = document.getElementById('newImage');
+const $description = document.getElementById('decription');
+const $camera = document.getElementById('camera');
+const $pauserCamCapture = document.getElementById('pauserCamCapture');
+const $imageCaptureDescription = document.getElementById('imageCaptureDescription');
+let cameraActive = false;
+image.src ='https://i.imgur.com/JlUvsxa.jpg';  
+$pauserCamCapture.addEventListener('click',()=>{
+  cameraActive = !cameraActive; 
  
+  if(cameraActive){
+    $pauserCamCapture.innerText = 'Desactivar Captura';
+  }else{
+    $pauserCamCapture.innerText = 'Activar Captura';
+  }
+  run();
+})
+const run = async() => {
+  try {
+    net = await mobilnet.load()
+    
+    const imageclasification =await net.classify(image) ;  
+    $description.innerHTML = `<p>${JSON.stringify(imageclasification)}</p>`;
+    const video = await tf.data.webcam($camera); 
+    while(cameraActive){
+      const imageCapture =await video.capture()
+      const imagecaptureClasification =await net.classify(imageCapture) ;  
+      $imageCaptureDescription.innerHTML = `<p>Prediccion: ${imagecaptureClasification[0].className}</p>
+                                            <p>probabilidad: ${imagecaptureClasification[0].probability}</hp>`
+      imageCapture.dispose();
+      //espera el procesamiento del frame para provesar el sigiente 
+      await tf.nextFrame();
+    }
+  } catch (error) {
+    console.log(error);
+    
+  }
+
+}
+
+ image.onload = async ()=>{
+  run()
+}
+
+$newImage.addEventListener('click',()=>{
+  const number = Math.floor(Math.random()*1000);
+  image.src = `https://picsum.photos/200/300?random=${number}`;
+})
+ 
+
 
